49f781ebc3592b88dc6602056c4b15dbda98feee
GabrielleFreno01 <freno.gabrielle@gmail.com>
2020-11-12 13:39:47 +0100

    test push

--- gmoria/lib/main.dart
+++ gmoria/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
+  //test run
 }
 
 class MyApp extends StatelessWidget {
--- gmory/android/app/src/main/kotlin/com/example/gmory/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.gmory
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    //Test push Mathieu
-}
